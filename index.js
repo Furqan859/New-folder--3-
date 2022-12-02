f1ddbabbb9514a3ee487669d5b29e764b9dfa306
Furqan-Rasool <furqanrasool2@gmail.com>
2022-12-02 18:14:39 +0500

    maximum sum subarray

---
+++ index.js
@@ -0,0 +1,34 @@
+const array = [-1,-2,3];
+let maxNum = 0;
+let previous = 0;
+if(array.length <= 0){
+    console.log(0)
+}else{
+    
+for(i=0;i<array.length;i++){
+
+    previous +=  array[i];
+
+    if(maxNum<previous){
+        maxNum = previous;
+        console.log(maxNum);
+    } if(previous = 0  ){ 
+        console.log(0)
+
+    }
+}
+}
+      
+       
+        
+
+        // const max = array.reduce((maxNum,previous)=>{
+        //     return Math.max(maxNum,previous);
+        // })
+        // max;
+        // console.log("maximum value",max);
+
+        
+
+
+
